5989130ff916c00c3bcaa7e6817d200855759567
noobMasterProgrammer <ghoshabhishek1640@gmail.com>
2023-02-14 22:54:31 +0530

    commit

--- arc-reactor-redis/src/main/java/com/tutorial/model/EnableCaching.java
+++
@@ -1,9 +0,0 @@
-package com.tutorial.model;
-
-import java.io.Serializable;
-
-public interface EnableCaching extends Serializable  {
-	String uniqueId();
-	String collectionName();
-	Object getPayload();
-}
